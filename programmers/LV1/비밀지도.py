d2c96d9343a376225b5cabd394dbdd7bdede02cb
ymink716 <ymink716@gmail.com>
2020-07-28 17:13:38 +0900

    프로그래머스 카카오 기출 lv1 문제풀이

--- programmers/LV1/비밀지도.py
+++ programmers/LV1/비밀지도.py
@@ -1,13 +1,13 @@
-# 비밀지도 (못품)
 def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
-        print(str(bin(arr1[i] | arr2[i]))[2:])
-        answer.append(str(bin(arr1[i] | arr2[i]))[2:].replace('1', '#').replace('0', ' '))
-    return answer  
-        
+        # 2진수 or 연산 -> zfill() 함수로 자리수 맞추기 -> #, " "로 대체
+        result = bin(arr1[i] | arr2[i])[2:].zfill(n).replace("1", "#").replace("0", " ")
+        answer.append(result)
+    return answer
 
 
-# 테스트
-#print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
+# test
+print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
 print(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]))
+print(solution(5, [0, 0, 0, 0, 0], [30, 1, 21, 17, 28]))
--- programmers/LV1/예산.py
+++ programmers/LV1/예산.py
@@ -1,12 +1,15 @@
-# 예산
 def solution(d, budget):
     d.sort()
-    for i in range(len(d)):
-        budget -= d[i]
-        if budget < 0: return i
-    return len(d)
+    cnt = 0
+    for v in d:
+        if v > budget:
+            break
+        cnt += 1
+        budget -= v
+    return cnt
 
+# 부서별 신청 금액을 정렬한 후 예산에서 하나씩 빼주면서 지원 횟수를 파악하는 방식으로 풀었다.
 
-# 테스트
+# test
 print(solution([1, 3, 2, 5, 4], 9))
 print(solution([2, 2, 3, 3], 10))
