f0d241346f53b5ec3fdb3810b08f64b56c9839fb
ymink716 <ymink716@gmail.com>
2023-07-24 15:08:45 +0900

    BOJ DP 문제 풀이

---
+++ BOJ/BaaarkingDog/0x10_다이나믹 프로그래밍/11727.py
@@ -0,0 +1,14 @@
+# 2×n 타일링 2
+# https://www.acmicpc.net/problem/11727
+
+n = int(input())
+
+dp = [0] * 1001
+
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3, n + 1):
+    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
+
+print(dp[n])
